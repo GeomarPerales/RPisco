079646c7d6c424ac63a92142eb9c11fb64cdd01d
Geomar Perales Apaico <per3app@gmail.com>
2021-05-02 16:13:19 -0500

    Add files via upload

---
+++ R/piscodgroup.R
@@ -0,0 +1,75 @@
+#' values extraction of PISCO daily precipitation from a stations group.
+#'
+#' function for extract values of PISCO daily precipitation from a stations group, PISCO is Peruvian Interpolated Data of the Senamhi’s Climatological and Hydrologycal Observations.
+#' @param x a dataframe with PISCO file name (in netCDF format), longitude and latitude of a stations group.
+#' @param start OPTIONAL, start date of dataset, default value is 1981-01-01.
+#' @param end OPTIONAL, end date of dataset, default value is 2016-12-31.
+#' @importFrom raster brick
+#' @importFrom raster projection
+#' @importFrom raster extract
+#' @importFrom sp coordinates
+#' @import sp
+#' @import raster
+#' @export
+#' @name piscodgroup
+
+piscodgroup <-function(x, ...) UseMethod("piscodgroup")
+
+piscodgroup <- function(x, start = NULL, end = NULL){
+  x <- x[,1:4]
+  colnames(x) <- c("nc", "name","v1", "v2")
+  if(x$v1[1] < x$v2[1]){
+    colnames(x) <- c("nc", "name","lon", "lat")
+  } else if(x$v1[1] > x$v2[1]){
+    colnames(x) <- c("nc", "name","lat", "lon")
+  }
+
+  file.nc <- unique(as.character(x$nc))
+  name <- as.character(x$name)
+  longitude <- as.numeric(x$lon)
+  latitude <- as.numeric(x$lat)
+
+  if(is.numeric(longitude) & is.numeric(latitude)){
+    coord <- data.frame(x = as.numeric(longitude), y = as.numeric(latitude))
+  }  else {
+    stop("coordinates not defined")
+  }
+
+  coord <- coord
+  variable.raster <- raster::brick(file.nc)
+  sp::coordinates(coord) <- ~ x + y
+  raster::projection(coord) <- raster::projection(variable.raster)
+  points <- raster::extract(variable.raster[[1]], coord, cellnumbers = T)[,1]
+  Pisco.data <- t(variable.raster[points])
+  study.range <- data.frame(Date = seq(from = as.Date("1981-01-01"), to = as.Date("2016-12-31"), by = "days"))
+
+  Pisco.data <- cbind(study.range, format(Pisco.data, scientific = F, digits = 2))
+
+  row.names(Pisco.data) <- seq(1, nrow(Pisco.data), 1)
+  colnames(Pisco.data) <- c("date", as.vector(name))
+
+  if(is.null(start) & is.null(end)){
+    return(Pisco.data)
+  } else if(!is.null(start)  & !is.null(end)){
+    if( sapply( start, function(x) !all(is.na(as.Date(as.character(x),format="%Y-%m-%d")))) == TRUE &
+        sapply( start, function(x) !all(is.na(as.Date(as.character(x),format="%Y-%m-%d")))) == TRUE){
+      start.out <- start
+      end.out <- end
+      Pisco.data <- Pisco.data[(Pisco.data$Days >= start.out & Pisco.data$Days <= end.out),]
+      return(Pisco.data)
+    } else {
+      stop("date format not recognized, date format is %Y-%m-%d ")
+    }
+
+  } else if(!is.null(start) & sapply( start,
+                                      function(x) !all(is.na(as.Date(as.character(x),format="%Y-%m-%d")))) == TRUE){
+    start.out <- start
+    end.out <- "2016-12-01"
+    Pisco.data <- Pisco.data[(Pisco.data$Days >= start.out & Pisco.data$Days <= end.out),]
+    return(Pisco.data)
+  } else {
+    stop("date format not recognized, date format is %Y-%m-%d ")
+  }
+}
+
+#' @rdname piscodgroup
