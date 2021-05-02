1323d5a49a34f0090162f22c871fb5692417dcd9
Geomar Perales Apaico <per3app@gmail.com>
2021-05-02 16:14:14 -0500

    Add files via upload

--- inst/doc/RPisco.R
+++ inst/doc/RPisco.R
@@ -45,26 +45,15 @@ head(Huarpa.stations)
 
 ## -----------------------------------------------------------------------------
 PISCO.file <- "D:\\PISCOm_pp_2.1.nc"
-
-monthly.data <- list()
-monthly.matrix <- data.frame(matrix(NA, 432, nrow(Huarpa.stations)))
-for (i in 1:nrow(Huarpa.stations)) {
-  monthly.data[[i]] <- piscom(data.frame(PISCO.file, Huarpa.stations$Lon[i], Huarpa.stations$Lat[i]))
-  monthly.matrix[,i] <- monthly.data[[i]]$values
-}
-colnames(monthly.matrix) <- Huarpa.stations$Estacion
-head(monthly.matrix)
+monthly.data <- piscomgroup(data.frame(PISCO.file, Huarpa.stations))
+head(monthly.data)
 
 ## -----------------------------------------------------------------------------
-ini.yr <- as.Date("1981-01-01")
-end.yr <- as.Date("2016-12-01")
-date <- seq(ini.yr, end.yr, by = "months")
-monthly.matrix <- data.frame(date, monthly.matrix)
-monthlyavg(data.frame(monthly.matrix$date, monthly.matrix[,2]))
+monthlyavg(data.frame(monthly.data$date, monthly.data[,2]))
 
 ## -----------------------------------------------------------------------------
-yearlyavg(data.frame(monthly.matrix$date, monthly.matrix[,2]))
+yearlyavg(data.frame(monthly.data$date, monthly.data[,2]))
 
 ## -----------------------------------------------------------------------------
-head(pisco2annual(data.frame(monthly.matrix$date, monthly.matrix[,2])))
+head(pisco2annual(data.frame(monthly.matrix$date, monthly.data[,2])))
 
