29270baf0e0702c4c3b6cbc7bb92c96e2891d90d
Geomar Perales Apaico <per3app@gmail.com>
2021-10-27 06:16:08 -0500

    Add files via upload

---
+++ R/piscod.eo.R
@@ -0,0 +1,54 @@
+#' values extraction of PISCO evapotranspiration daily data
+#'
+#' function for extract values of PISCO evapotranspiration daily data, PISCO is Peruvian Interpolated Data of the Senamhi’s Climatological and Hydrologycal Observations.
+#' @param x a dataframe with PISCO files folder (in netCDF format), longitude and latitude of station.
+#' @importFrom raster brick
+#' @importFrom raster projection
+#' @importFrom raster extract
+#' @importFrom raster readAll
+#' @importFrom sp coordinates
+#' @import sp
+#' @import raster
+#'
+#' @export
+#'
+#' @author Geomar Perales Apaico
+#'
+#' @name piscod.eo
+
+piscod.eo <-function(x, ...) UseMethod("piscod")
+
+piscod.eo <- function(x){
+  x <- x[,1:3]
+  colnames(x) <- c("nc","v1", "v2")
+  if(x$v1[1] < x$v2[1]){
+    colnames(x) <- c("nc","lon", "lat")
+  } else if(x$v1[1] > x$v2[1]){
+    colnames(x) <- c("nc","lat", "lon")
+  }
+
+  longitude <- as.numeric(x$lon)
+  latitude <- as.numeric(x$lat)
+  if(is.numeric(longitude) & is.numeric(latitude)){
+    coord <- data.frame(x = as.numeric(longitude), y = as.numeric(latitude))
+  }  else {
+    stop("coordinates not defined")
+  }
+
+  dir.nc <- as.character(x$nc)
+  files.ncdf <- list.files(path = dir.nc, pattern = ".nc")
+
+  sp::coordinates(coord) <- ~ x + y
+  Pisco.data <- list()
+  for (i in 1:length(files.ncdf)) {
+    file.nc <- as.character(paste0(dir.nc, "\\", files.ncdf[i]))
+    variable.raster <- raster::brick(file.nc)
+    raster::projection(coord) <- raster::projection(variable.raster)
+    points <- raster::extract(readAll(variable.raster[[1]]), coord, cellnumbers = T)[,1]
+    Pisco.data[[i]] <- t(variable.raster[points])
+  }
+  Pisco.df <- do.call( rbind, Pisco.data)
+  return(Pisco.df)
+}
+
+#' @rdname piscod.eo
